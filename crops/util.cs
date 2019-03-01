3f093bf5ed37e725beb6723f4c388ad057852145
ConanODoyle <samuel.w.sze@gmail.com>
2019-03-01 09:14:08 -0800

    Move fillwater to util.cs

--- crops/util.cs
+++ crops/util.cs
@@ -1,28 +1,45 @@
 function totalWater(%type, %startStage)
 {
-  %ticks = $Farming::Crops::plantData_[%type, %startStage, "numGrowTicks"];
-  %time = $Farming::Crops::plantData_[%type, %startStage, "timePerTick"];
-  %water = $Farming::Crops::plantData_[%type, %startStage, "waterPerTick"];
-  if (%time <= 0)
-  {
-    return 0;
-  }
-  else
-  {
-    return %water * %ticks + totalWater(%type, %startStage + 1);
-  }
+	%ticks = $Farming::Crops::plantData_[%type, %startStage, "numGrowTicks"];
+	%time = $Farming::Crops::plantData_[%type, %startStage, "timePerTick"];
+	%water = $Farming::Crops::plantData_[%type, %startStage, "waterPerTick"];
+	if (%time <= 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return %water * %ticks + totalWater(%type, %startStage + 1);
+	}
 }
 
 function totalTime(%type, %startStage)
 {
-  %ticks = $Farming::Crops::plantData_[%type, %startStage, "numGrowTicks"];
-  %time = $Farming::Crops::plantData_[%type, %startStage, "timePerTick"];
-  if (%time <= 0)
-  {
-    return 0;
-  }
-  else
-  {
-    return %time * %ticks + totalTime(%type, %startStage + 1);
-  }
+	%ticks = $Farming::Crops::plantData_[%type, %startStage, "numGrowTicks"];
+	%time = $Farming::Crops::plantData_[%type, %startStage, "timePerTick"];
+	if (%time <= 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return %time * %ticks + totalTime(%type, %startStage + 1);
+	}
+}
+
+function setAllWaterLevelsFull()
+{
+	for (%i = 0; %i < MainBrickgroup.getCount(); %i++)
+	{
+		%group = MainBrickgroup.getObject(%i);
+
+		for (%j = 0; %j < %group.getCount(); %j++)
+		{
+			%brick = %group.getObject(%j);
+			if (%brick.getDatablock().isWaterTank || %brick.getDatablock().isDirt)
+			{
+				%brick.setWaterLevel(100000);
+			}
+		}
+	}
 }
--- debug.cs
+++ debug.cs
@@ -10,23 +10,6 @@ function loadLastAutosave()
 }
 //DO NOT schedule the growTick call - it will break loading plants!
 
-function setAllWaterLevelsFull()
-{
-  for (%i = 0; %i < MainBrickgroup.getCount(); %i++)
-  {
-    %group = MainBrickgroup.getObject(%i);
-
-    for (%j = 0; %j < %group.getCount(); %j++)
-    {
-      %brick = %group.getObject(%j);
-      if (%brick.getDatablock().isWaterTank || %brick.getDatablock().isDirt)
-      {
-        %brick.setWaterLevel(100000);
-      }
-    }
-  }
-}
-
 package brickDeath
 {
   function fxDTSBrick::onDeath(%obj)
