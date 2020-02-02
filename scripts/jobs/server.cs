273d4181551af6c6a736c0dc1ccd93beaa2989e7
ConanODoyle <samuel.w.sze@gmail.com>
2020-02-02 14:46:32 -0800

    Add new posting brick files

---
+++ scripts/jobs/server.cs
@@ -0,0 +1,69 @@
+datablock fxDTSBrickData(brickBulletinBoardData)
+{
+	brickFile = "./resources/bulletinboard.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Bulletin Board";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting1Data)
+{
+	brickFile = "./resources/posting1.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting1";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting2Data)
+{
+	brickFile = "./resources/posting2.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting2";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting3Data)
+{
+	brickFile = "./resources/posting3.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting3";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting4Data)
+{
+	brickFile = "./resources/posting4.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting4";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting5Data)
+{
+	brickFile = "./resources/posting5.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting5";
+	cost = -1;
+};
+
+datablock fxDTSBrickData(brickPosting6Data)
+{
+	brickFile = "./resources/posting6.blb";
+	
+	category = "Farming";
+	subCategory = "Bulletin Board Test";
+	uiName = "Posting6";
+	cost = -1;
+};
