30488e52be1ac27e1ce466c5b36b7cfb0f25bd4a
ConanODoyle <samuel.w.sze@gmail.com>
2020-08-25 03:56:03 -0700

    Update centerprint menu to v2

--- lib/Support_CenterprintMenuSystem.cs
+++ lib/Support_CenterprintMenuSystem.cs
@@ -1,6 +1,63 @@
 //centerprint menu system
 //modular system written by conan
+//version 2.0
+//- added justify, fontA/B support, playShiftAudio, playSelectAudio
+//- added basic documentation
 
+
+//to use:
+//reference the example centerprint menu ScriptObjects below, as well as the fields
+//menus are ScriptObjects with fields
+//menu shows up to 5 options at a time, and scrolls the menu as the player shifts their selection
+//loops back when going past the end of the menu - going up from the first selection puts you at the last one, and vice versa
+//
+//to start, call %client.startCenterprintMenu(%menuObject);
+//to exit, call %client.exitCenterprintMenu();
+//
+//centerprint menu is automatically exited when an option is selected
+//menu is controlled with brick controls and is automatically exited on death and respawn
+
+
+//menu fields:
+//
+//isCenterprintMenu - must be true for it to be displayed
+//menuName - the displayed name of the menu
+//menuOption[#] - the displayed name of the menu option
+//menuFunction[0] - the function called when that selection is confirmed
+//					receives the client, menu, and option # as parameters, in that order
+//					can be left empty if no callback is needed
+//
+//justify - <just:left> or <just:right> the menu. by default it is <just:center>
+//fontA - font of not-current selection
+//fontB - font of current selection
+//playShiftAudio - plays MsgUploadEnd tone when changing options
+//playSelectAudio - plays MsgAdminForce tone when selecting an option
+//
+//deleteOnExit - deletes ScriptObject on menu exit. scheduled
+
+
+//suggested functions to package, if extending or modifying functionality:
+//GameConnection::displayCenterprintMenu(%client, %diff)
+//	- called every time the menu is refreshed and displayed to client (selection changes)
+//	- diff is # of options moved up or down 
+//playCenterprintMenuSound(%client, %string)
+//	- called when the menu wants to play a sound 
+//	- defaults: 'MsgAdminForce' for confirming selection, 'MsgUploadEnd' for moving selection
+//	- only called if playSelectAudio, playShiftAudio are enabled (respectively)
+
+
+$CenterprintMenuSystemVersion += 0; //make sure its a valid number
+if ($CenterprintMenuSystemVersion < 2.0 && isFunction(GameConnection, startCenterprintMenu))
+{
+	echo("Support_CenterprintMenuSystem: Older centerprint menu system detected: v" @ $CenterprintMenuSystemVersion);
+	echo("Support_CenterprintMenuSystem: Overwriting with version 2.0");
+}
+else if ($CenterprintMenuSystemVersion >= 2.0)
+{
+	echo("Support_CenterprintMenuSystem: Centerprint menu system detected: v" @ $CenterprintMenuSystemVersion);
+	echo("Support_CenterprintMenuSystem: Skipping... (v2.0)");
+}
+$CenterprintMenuSystemVersion = 2.0;
 
 if (!isObject(exampleCenterprintMenu))
 {
@@ -15,6 +72,8 @@ if (!isObject(exampleCenterprintMenu))
 		menuFunction[1] = "option2Func";
 		menuOption[2] = "Option 3";
 		menuFunction[2] = "option3Func";
+
+		justify = "<just:right>";
 
 		//deleteOnExit = 1;
 
@@ -45,136 +104,158 @@ if (!isObject(exampleCenterprintMenu2))
 		menuOption[6] = "Option 6";
 		menuFunction[6] = "option6Func";
 
+		justify = ""; //defaults to <just:center>
+
 		menuOptionCount = 7;
 	};
 	// MissionCleanup.add(exampleCenterprintMenu2);
 }
 
 //core//
+//put in package to override previous versions which did not have version checking prior to overwrite 
 
-function GameConnection::startCenterprintMenu(%cl, %menuObj)
+package Support_CenterprintMenuSystemPackage 
 {
-	if (!isObject(%menuObj))
+	function GameConnection::startCenterprintMenu(%cl, %menuObj)
 	{
-		error("ERROR: startCenterprintMenu cannot find menu \"" @ %menuObj @ "\" to display!");
-		return;
+		if (!isObject(%menuObj))
+		{
+			error("ERROR: startCenterprintMenu cannot find menu \"" @ %menuObj @ "\" to display!");
+			return;
+		}
+		else if (!%menuObj.isCenterprintMenu)
+		{
+			error("ERROR: %menuObj not a centerprint menu!");
+			return;
+		}
+
+		if (%cl.lastMessagedBrickControls + 5 < $Sim::Time)
+		{
+			messageClient(%cl, '', "\c5Use brick controls/numpad to navigate the centerprint menu.");
+		}
+		%cl.lastMessagedBrickControls = $Sim::Time;
+
+		%cl.isInCenterprintMenu = 1;
+		%cl.centerprintMenu = %menuObj.getID();
+		%cl.currOption = 0;
+
+		%cl.displayCenterprintMenu();
 	}
-	else if (!%menuObj.isCenterprintMenu)
-	{
-		error("ERROR: %menuObj not a centerprint menu!");
-		return;
-	}
 
-	if (%cl.lastMessagedBrickControls + 5 < $Sim::Time)
-	{
-		messageClient(%cl, '', "\c5Use brick controls to navigate the centerprint menu.");
-	}
-	%cl.lastMessagedBrickControls = $Sim::Time;
-
-	%cl.isInCenterprintMenu = 1;
-	%cl.centerprintMenu = %menuObj.getID();
-	%cl.currOption = 0;
-
-	%cl.displayCenterprintMenu();
-}
-
-function GameConnection::exitCenterprintMenu(%cl)
-{
-	%cl.centerprint("", 1);
-	%cl.isInCenterprintMenu = 0;
-	%menu = %cl.centerprintMenu;
-	%cl.centerprintMenu = "";
-
-	if (%menu.deleteOnExit)
-	{
-		%menu.schedule(1000, delete);
-	}
-}
-
-function GameConnection::displayCenterprintMenu(%cl, %diff)
-{
-	if (!isObject(%cl.centerprintMenu) || !%cl.isInCenterprintMenu)
+	function GameConnection::exitCenterprintMenu(%cl)
 	{
 		%cl.centerprint("", 1);
-		return;
-	}
+		%cl.isInCenterprintMenu = 0;
+		%menu = %cl.centerprintMenu;
+		%cl.centerprintMenu = "";
 
-	%menu = %cl.centerprintMenu;
-
-	%menuName = %menu.menuName;
-	%optionIDX = %cl.currOption = %cl.currOption + 0; //make sure its a number and not empty string
-	if (%diff < 0)
-	{
-		%diff = %menu.menuOptionCount + %diff;
-	}
-	%optionIDX = %cl.currOption = (%optionIDX + %diff) % %menu.menuOptionCount;
-
-	if (%diff != 0)
-	{
-		messageClient(%cl, 'MsgUploadEnd');
-	}
-
-	//display up to 5 options
-	%min = %optionIDX;
-	%max = %optionIDX;
-	%dist = %max - %min;
-	while (%dist < 4)
-	{
-		if (%min > 0)
-			%min--;
-		if (%max < %menu.menuOptionCount - 1)
-			%max++;
-
-		if (%min == 0 && %max == %menu.menuOptionCount - 1) //menu is less than 5 options
-			%dist = 50;
-		else
-			%dist = %max - %min;
-	}
-
-	%fontA = "<font:Arial:18>\c6";
-	%fontB = "<font:Arial Bold:18>\c3";
-
-	%line = "<just:right>\c3" @ %menuName @ " <br>" @ %fontA;
-	for (%i = 0; %i <= getMin(%dist, 6); %i++)
-	{
-		%option = %i + %min;
-		if (%option == %optionIDX)
+		if (%menu.deleteOnExit)
 		{
-			%line = %line @ %fontB @ %menu.menuOption[%option] @ " <br>" @ %fontA;
-		}
-		else
-		{
-			%line = %line @ %menu.menuOption[%option] @ " <br>\c6";
+			%menu.schedule(1, delete);
 		}
 	}
 
-	%cl.centerprint(%line);
-}
-
-function GameConnection::confirmCenterprintMenu(%cl)
-{
-	if (!%cl.isInCenterprintMenu)
+	function GameConnection::displayCenterprintMenu(%cl, %diff)
 	{
-		return;
+		if (!isObject(%cl.centerprintMenu) || !%cl.isInCenterprintMenu)
+		{
+			%cl.centerprint("", 1);
+			return;
+		}
+
+		%menu = %cl.centerprintMenu;
+
+		%menuName = %menu.menuName;
+		%optionIDX = %cl.currOption = %cl.currOption + 0; //make sure its a number and not empty string
+		if (%diff < 0)
+		{
+			%diff = %menu.menuOptionCount + %diff;
+		}
+		%optionIDX = %cl.currOption = (%optionIDX + %diff) % %menu.menuOptionCount;
+
+		if (%diff != 0 && %menu.playShiftAudio)
+		{
+			playCenterprintMenuSound(%cl, 'MsgUploadEnd');
+		}
+
+		//display up to 5 options
+		%min = %optionIDX;
+		%max = %optionIDX;
+		%dist = %max - %min;
+		while (%dist < 4)
+		{
+			if (%min > 0)
+				%min--;
+			if (%max < %menu.menuOptionCount - 1) {
+                
+				%max++;
+            }
+            
+			if (%min == 0 && %max == %menu.menuOptionCount - 1) //menu is less than 5 options
+				%dist = 50;
+			else
+				%dist = %max - %min;
+		}
+
+		%fontA = %menu.fontA $= "" ? "<font:Arial:18>\c6" : %menu.fontA;
+		%fontB = %menu.fontB $= "" ? "<font:Arial Bold:18>\c3" : %menu.fontB;
+
+		%line = %menu.justify @ "\c3" @ %menuName @ " <br>" @ %fontA;
+		for (%i = 0; %i <= getMin(%dist, 6); %i++)
+		{
+			%option = %i + %min;
+			if (%option == %optionIDX)
+			{
+				%line = %line @ %fontB @ %menu.menuOption[%option] @ " <br>" @ %fontA;
+			}
+			else
+			{
+				%line = %line @ %menu.menuOption[%option] @ " <br>\c6";
+			}
+		}
+
+		%cl.centerprint(%line);
 	}
 
-	%menu = %cl.centerprintMenu;
-	%option = %cl.currOption;
-
-	%cl.exitCenterprintMenu();
-	%func = %menu.menuFunction[%option];
-	// messageClient(%cl, 'MsgAdminForce');
-	if (!isFunction(%func))
+	function GameConnection::confirmCenterprintMenu(%cl)
 	{
-		// error("ERROR: confirmCenterprintMenu: cannot find function " @ %func @ "!");
-		return;
-	}
-	else
-	{
-		call(%func, %cl, %menu, %option);
-	}
-}
+		if (!%cl.isInCenterprintMenu)
+		{
+			return;
+		}
 
+		%menu = %cl.centerprintMenu;
+		%option = %cl.currOption;
+
+		%cl.exitCenterprintMenu();
+		%func = %menu.menuFunction[%option];
+		if (%menu.playSelectAudio)
+		{
+			playCenterprintMenuSound(%cl, 'MsgAdminForce');
+		}
+
+		if (%func !$= "" && !isFunction(%func))
+		{
+			error("ERROR: confirmCenterprintMenu: cannot find function " @ %func @ "!");
+			return;
+		}
+		else
+		{
+			call(%func, %cl, %menu, %option);
+		}
+	}
+
+	function playCenterprintMenuSound(%cl, %sound)
+	{
+		%ret = parent::playCenterprintMenuSound(%cl, %sound);
+
+		if (!%ret)
+		{
+			messageClient(%cl, %sound);
+		}
+	}
+};
+activatePackage(Support_CenterprintMenuSystemPackage);
 
 //package//
 
