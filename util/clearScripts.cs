a0daf29b9bc58712975035444dfefc1b4e4114c5
ConanODoyle <samuel.w.sze@gmail.com>
2020-08-26 13:36:20 -0700

    Fix minor bug

--- util/clearScripts.cs
+++ util/clearScripts.cs
@@ -1,14 +1,14 @@
 function servercmdclearitems(%cl)
 {
-  if (%cl.isAdmin)
-  {
-    %pos = %cl.player.position;
-    initcontainerradiussearch(%pos, 300, $Typemasks::itemobjecttype);
-    while (isobject(%next = containerSearchNext()))
-    {
-      if (!%next.isStatic())
-        %next.delete();
-    }
-  }
-  messageAll('MsgClearBricks', "\c2" @%cl.name @ " cleared nearby items.");
+	if (%cl.isAdmin)
+	{
+		%pos = %cl.player.getPosition();
+		initContainerRadiusSearch(%pos, 300, $Typemasks::itemobjecttype);
+		while (isObject(%next = containerSearchNext()))
+		{
+			if (!%next.isStatic())
+				%next.delete();
+		}
+		messageAll('MsgClearBricks', "\c2" @%cl.name @ " cleared nearby items.");
+	}
 }
