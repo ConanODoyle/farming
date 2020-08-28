88a041f794268b0269a28371cb49164cdaf0cb11
ConanODoyle <samuel.w.sze@gmail.com>
2020-08-28 09:54:53 -0700

    Fix git pull bugs

--- util/gitpull.cs
+++ util/gitpull.cs
@@ -4,16 +4,33 @@ function serverCmdGitPull(%cl)
 	{
 		return;
 	}
-	messageAll('', "Starting git pull...");
+	talk("\c3" @ %cl.name @ " - Starting git pull...");
 	if (isObject($gitPullTCP))
 	{
 		$gitPullTCP.delete();
 	}
-	$gitPullTCP = TCPClient("GET", "155.138.204.83", "80", "pullfarming", "", "TCPGitPullObj", "");
+	$gitPullTCP = connectToURL("155.138.204.83/pullfarming", "GET", "", "TCPGitPullObj", "");
 }
 
 function TCPGitPullObj::onLine(%this, %line)
 {
-	talk("line " @ %this.linecount + 0 @ ": " @ %line);
+	if ($debugGitPullTCP)
+	{
+		talk("line " @ %this.linecount + 0 @ ": " @ %line);
+	}
 	%this.linecount++;
+}
+
+function TCPGitPullObj::onDone(%this, %error)
+{
+	if (%error)
+	{
+		talk("gitpull: Error in connecting to server!");
+		return;
+	}
+	else
+	{
+		setModPaths(getModPaths());
+		schedule(1000, 0, talk, "\c3Git pull complete!");
+	}
 }
