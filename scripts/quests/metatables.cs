a01ab69e7f4ebd5f189401f329cee7ea0a208ca9
irrelevantirreverent <irrelevant.irreverent@gmail.com>
2020-08-29 00:26:11 -0400

    Update metatable description

--- scripts/quests/metatables.cs
+++ scripts/quests/metatables.cs
@@ -1,12 +1,21 @@
-// TODO: manually create a list of metatables for quest-givers
+// TODO: manually create a list of "metatables" for quest-givers
+//
+// metatables are just a string of table names so "vegetableTableA fruitTableA
+// fishTableA" might be a valid metatable
 //
 // the sort of thing i'm looking for:
-// quest request metatable for each difficulty level, potentially one for each quest-giver, but only one is really necessary
-// quest reward metatable for each difficulty level, one for each quest-giver
+// quest request metatable for each difficulty level, potentially one set for
+// each quest-giver
+// quest reward metatable for each difficulty level, definitely one set for each
+// quest-giver
 //
-// possible considerations: metatables of various types e.g. fish, crops, minerals
-// notes: metatables should be essentially just a way to split up tables into at least 3 categories so that players get quests with varied requests and rewards
-// more categories is better and will enhance variety of quests
-// tables can be members of more than one metatable, but metatables' tables should not have any intersections
-// i.e. a metatable containing two tables should not have potatoes in both of those tables
-// if this happens, a quest might be given that requests or rewards
+// notes: metatables are just a way to combine tables of various categories into
+// a larger table so that players can get enhanced variety of requests - how
+// these are split up is arbitrary
+//
+// metatables must contain at least 3 tables so that quests can roll up to 3
+// unique item types without running out of tables to draw from
+//
+// tables can be members of more than one metatable, but metatables' tables
+// should not have any common items, or else quests like "get 75 potatoes and
+// 32 potatoes" would happen because of the way quest generation works
