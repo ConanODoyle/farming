65637c2fc9b7519f52af63a78b68d471ec105b1c
ConanODoyle <samuel.w.sze@gmail.com>
2020-10-04 22:27:45 -0700

    Add repeat condition to watering tool messages

--- scripts/wateringTools.cs
+++ scripts/wateringTools.cs
@@ -14,9 +14,23 @@ package wateringCat
 			}
 		}
 
-		if (%hasWateringCat && getRandom() < 0.15 && strPos(%msg, ":0") < 0)
+		if (strPos(strLwr(%msg), "again") >= 0 && %cl.didCatMessage)
+		{
+			%chance = 1;
+		}
+		else
+		{
+			%chance = 0.15;
+		}
+
+		if (%hasWateringCat && getRandom() < %chance && strPos(%msg, ":0") < 0)
 		{
 			%msg = %msg SPC ":0";
+			%cl.didCatMessage = 1;
+		}
+		else
+		{
+			%cl.didCatMessage = 0;
 		}
 
 		return parent::serverCmdMessageSent(%cl, %msg);
@@ -40,9 +54,23 @@ package wateringSnake
 			}
 		}
 
-		if (%hasWateringCat && getRandom() < 0.15 && strPos(%msg, ":V") < 0)
+		if (strPos(strLwr(%msg), "again") >= 0 && %cl.didSnakeMessage)
+		{
+			%chance = 1;
+		}
+		else
+		{
+			%chance = 0.15;
+		}
+
+		if (%hasWateringCat && getRandom() < %chance && strPos(%msg, ":V") < 0)
 		{
 			%msg = %msg SPC ":V";
+			%cl.didSnakeMessage = 1;
+		}
+		else
+		{
+			%cl.didSnakeMessage = 0;
 		}
 
 		return parent::serverCmdMessageSent(%cl, %msg);
