96fa10afb5d85f2a9ce81da0864d6b4afa1e474f
ConanODoyle <samuel.w.sze@gmail.com>
2020-10-11 22:22:23 -0700

    Add new dialogue objects

--- scripts/modules.cs
+++ scripts/modules.cs
@@ -5,6 +5,7 @@ echo("--Loading farm modules");
 exec("./infoBubbles/server.cs");
 exec("./travel/server.cs");
 exec("./botDeals/server.cs");
+exec("./botDialogues/server.cs");
 exec("./botShop/server.cs");
 exec("./processors/server.cs");
 exec("./playerShops/server.cs");
