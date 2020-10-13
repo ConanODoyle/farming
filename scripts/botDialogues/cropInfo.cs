b289ee8ce9ea738f770092aa64fa5d0a5d437431
ConanODoyle <samuel.w.sze@gmail.com>
2020-10-13 01:02:05 -0700

    Write crop info dialogue

--- scripts/botDialogues/cropInfo.cs
+++ scripts/botDialogues/cropInfo.cs
@@ -110,15 +110,31 @@ function setupPurchase(%dataObj)
 	
 	switch$ (%crop)
 	{
-		case "Potato": %v1 = "var";
+		case "Potato": 
+			%s1 = "Potatoes are very quick to grow, are harvested once, and drop between 2-4 potatoes and usually a seed.";
+			%s2 = "Be careful not to let them grow too long or else they'll drop less!";
 		case "Carrot":
+			%s1 = "Carrots are quick to grow, are harvested once, and drop between 2-3 carrots and usually a seed.";
+			%s2 = "If they grow too long, they'll drop less, so keep an eye on them!";
 		case "Onion":
+			%s1 = "Onions take a while to grow, but give a lot of experience and can be carried in big stacks. They drop 3-5 onions.";
+			%s2 = "You can harvest them early, but you'll get fewer onions and a lower chance for a seed drop.";
 		case "Turnip":
+			%s1 = "Turnips are profitable cash crops, but take a decent bit to grow, and don't stack well.";
+			%s2 = "I hear sometimes they can look like they're staring at you...";
 		case "Portobello":
+			%s1 = "Portobellos are mushrooms that are sensitive to light. They even grow with no light!";
+			%s2 = "That said, they still want some light - imperfect lighting will slow their growth significantly.";
 
 		case "Tomato":
+			%s1 = "Tomatoes are cheap crops that can be harvested up to 12 times. They drop 2-5 per harvest.";
+			%s2 = "They cost 10 experience to plant, and give 0-1 experience per harvest.";
 		case "Corn":
+			%s1 = "Corn can be harvested up to 3 times and drop 3-4 per harvest, but need nutrients to reach their final stage.";
+			%s2 = "They cost 9 experience to plant, and give 2-3 experience per harvest.";
 		case "Wheat":
+			%s1 = "Wheat is a more compact alternative to corn, dropping 3-5 per harvest. They take slightly longer to grow as well.";
+			%s2 = "They cost 12 experience to plant, and give 2-3 experience per harvest.";
 		case "Cabbage":
 		case "Blueberry":
 		case "Chili":
