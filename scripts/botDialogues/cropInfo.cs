f51e62f13a7ee696fb19bf5fcf5c67bcf14d473d
ConanODoyle <samuel.w.sze@gmail.com>
2020-10-13 21:43:02 -0700

    Continue work on crop info, slight rebalancing of crops

--- scripts/botDialogues/cropInfo.cs
+++ scripts/botDialogues/cropInfo.cs
@@ -123,8 +123,8 @@ function setupPurchase(%dataObj)
 			%s1 = "Turnips are profitable cash crops, but take a decent bit to grow, and don't stack well.";
 			%s2 = "I hear sometimes they can look like they're staring at you...";
 		case "Portobello":
-			%s1 = "Portobellos are mushrooms that are sensitive to light. They even grow with no light!";
-			%s2 = "That said, they still want some light - imperfect lighting will slow their growth significantly.";
+			%s1 = "Portobellos are mushrooms that are sensitive to light. They even grow with no light, but need nutrients to grow.";
+			%s2 = "However, they still want some light - wrong lighting will slow their growth. Nutrients will speed it up.";
 
 		case "Tomato":
 			%s1 = "Tomatoes are cheap crops that can be harvested up to 12 times. They drop 2-5 per harvest.";
@@ -134,10 +134,16 @@ function setupPurchase(%dataObj)
 			%s2 = "They cost 9 experience to plant, and give 2-3 experience per harvest.";
 		case "Wheat":
 			%s1 = "Wheat is a more compact alternative to corn, dropping 3-5 per harvest. They take slightly longer to grow as well.";
-			%s2 = "They cost 12 experience to plant, and give 2-3 experience per harvest.";
+			%s2 = "They cost 12 experience to plant, and give 3-4 experience per harvest. It also needs nutrients to grow fully.";
 		case "Cabbage":
+			%s1 = "Cabbage is a water hungry crop, and drops 2-3 per harvest. Unlike corn and wheat, they don't need nutrients.";
+			%s2 = "You can harvest them 4 times, returning 3-5 exp per harvest. They cost 20 experience to plant.";
 		case "Blueberry":
+			%s1 = "Blueberries are the alternative version of turnips, able to be harvested 3 times, for 2-6 berries per harvest.";
+			%s2 = "However, they need nutrients to grow fully. They cost 20 experience to plant, and give 5-6 per harvest.";
 		case "Chili":
+			%s1 = "Chilis are a desert crop, using very little water. They need nutrients to fully grow, and drop 2-5 per harvest.";
+			%s2 = "They can be harvested 2 times, costing 12 experience and returning 5-6 per harvest.";
 		case "Watermelon":
 
 		case "Cactus":
