5353f1e326c8273720e2e21fa0ab6dd8f28f5c01
ConanODoyle <samuel.w.sze@gmail.com>
2020-10-26 20:14:00 -0700

    Fix syntax error

--- lib/zoneBrick1x4x5Fix.cs
+++ lib/zoneBrick1x4x5Fix.cs
@@ -1,10 +1,10 @@
-schedule(100, 0, eval,
-	"package ZoneBricks
-	{
-		function brickzone1x4x5::onLoadplant(%data,%this)
-		{	
-			Parent::onLoadPlant(%this);
-			brickzone1x4x5::LoadParameters(%this);
-		}
-	};"
-);
+$str = "";
+$str = $str SPC "package ZoneBricks {";
+$str = $str SPC "	function brickzone1x4x5::onLoadplant(%data,%this)";
+$str = $str SPC "	{";
+$str = $str SPC "		Parent::onLoadPlant(%this);";
+$str = $str SPC "		brickzone1x4x5::LoadParameters(%this);";
+$str = $str SPC "	}";
+$str = $str SPC "};";
+
+schedule(100, 0, eval, $str);
