e6902f476a157f4b649f0ec247c8212762313827
irrelereverent <irrelevant.irreverent@gmail.com>
2020-10-31 21:10:46 -0400

    Add tree clipper to tool type list

--- config/storage.cs
+++ config/storage.cs
@@ -58,7 +58,7 @@ $StorageTypeCropsList = "Potato\tCarrot\tTomato\tCorn\tWheat\tCabbage\tOnion\tBl
 $StorageTypeCropsIsBlacklist = false;
 
 $StorageTypeToolsExists = true;
-$StorageTypeToolsList = "toolgunItem\tHammerItem\tWrenchItem\tPrintGun\tSprinklerLinkItem\tElectricalCableItem\tPickaxeItem\tWateringCanItem\tWateringCan2Item\tWateringCan3Item\tWateringCatItem\tHoseItem\tHoseV2Item\tWeedWhackerItem\tWeedKiller\tPlanterItem\tPlanterV2Item\tTrowelItem\tClipperItem\tHoeItem\tSickleItem\tOrganicAnalyzerItem";
+$StorageTypeToolsList = "toolgunItem\tHammerItem\tWrenchItem\tPrintGun\tSprinklerLinkItem\tElectricalCableItem\tPickaxeItem\tWateringCanItem\tWateringCan2Item\tWateringCan3Item\tWateringCatItem\tHoseItem\tHoseV2Item\tWeedWhackerItem\tWeedKiller\tPlanterItem\tPlanterV2Item\tTrowelItem\tClipperItem\tTreeClipperItem\tHoeItem\tSickleItem\tOrganicAnalyzerItem";
 $StorageTypeToolsIsBlacklist = false;
 
 $StorageTypeQuestsExists = true;
