c9039125ef3c7657ee439c9ceea7ca0b17cd27ae
ConanODoyle <samuel.w.sze@gmail.com>
2020-11-17 22:16:28 -0800

    Update price list with new prices

--- config/prices.cs
+++ config/prices.cs
@@ -1,7 +1,7 @@
 JeepVehicle.uiName = "Jeep - $700";
 HorseArmor.uiName = "Horse - $200";
 
-
+//Vehicles
 $BuyCost_["Jeep_DASH_700"] = 700;
 $BuyCost_["Horse_DASH_200"] = 200;
 $BuyCost_["Storage_Cart_DASH_250"] = 250;
@@ -9,32 +9,64 @@ $BuyCost_["Large_Storage_Cart_2x_DASH_1200"] = 1200;
 $BuyCost_["Horse_Cart_DASH_2000"] = 2000;
 
 
+
+
+//Tools
 $BuyCost_["Trowel"] = 400;
 $BuyCost_["Clipper"] = 400;
 $BuyCost_["Tree_Clipper"] = 1400;
 $BuyCost_["Hoe"] = 1200;
 $BuyCost_["Sickle"] = 1200;
 
-$BuyCost_["Seed_Planter"] = 1000;
+$BuyCost_["Seed_Planter"] = 1200;
 $BuyCost_["Seed_Planter_V2"] = 2000;
 $BuyCost_["Seed_Reclaimer"] = 500;
 
-$BuyCost_["Watering_Can_v2"] = 400;
-$BuyCost_["Watering_Can_v3"] = 1200;
-$BuyCost_["Watering_Cat"] = 3200;
-$BuyCost_["Hose"] = 2800;
-$BuyCost_["Hose_V2"] = 4000;
+$BuyCost_["Shovel"] = 200;
+$BuyCost_["Weed_Cutter"] = 200;
+$BuyCost_["Organic_Analyzer"] = 700;
 
-$BuyCost_["Compost_Bin"] = 800;
+//Extra stackable items
+$BuyCost_["Fertilizer"] = 1.00;
+$BuyCost_["Weed_Killer"] = 0.50;
+$BuyCost_["Compost"] = 0.50;
+$BuyCost_["Phosphate"] = 0.50;
+$BuyCost_["Coal"] = 0.50;
+
+
+
+
+//Watering Tools
+$BuyCost_["Watering_Can_v2"] = 500;
+$BuyCost_["Watering_Can_v3"] = 850;
+$BuyCost_["Watering_Cat"] = 1200;
+$BuyCost_["Hose"] = 1000;
+$BuyCost_["Hose_V2"] = 2000;
+$BuyCost_["Watering_Snake"] = 2500;
+
+
+
+
+//Processors
+$BuyCost_["Compost_Bin"] = 600;
 $BuyCost_["Large_Compost_Bin"] = 1600;
 
 $BuyCost_["Flower_Pot"] = 100;
 $BuyCost_["Planter_Box"] = 300;
 
+
+
+
+//Electrical
 $BuyCost_["Coal_Generator"] = 600;
 $BuyCost_["Power_Control_Box"] = 400;
 $BuyCost_["Battery"] = 300;
 $BuyCost_["IndoorLightItem"] = 200;
+
+$BuyCost_["Medium_Tank_Pump"] = 2000;
+$BuyCost_["Large_Tank_Pump"] = 3500;
+
+
 
 
 //Produce
@@ -54,6 +86,9 @@ $BuyCost_["Cactus"] = 2.50;
 $BuyCost_["Watermelon"] = 8.00;
 $BuyCost_["Date"] = 18.00;
 $BuyCost_["Peach"] = 12.40;
+
+
+
 
 //Seeds
 $BuyCost_["PotatoSeed"] = 1.00;
@@ -77,10 +112,9 @@ $BuyCost_["DaisySeed"] = 1.00;
 $BuyCost_["LilySeed"] = 1.00;
 $BuyCost_["RoseSeed"] = 1.00;
 
-//Extra stackable items
 
-$BuyCost_["Fertilizer"] = 1.60;
-$BuyCost_["Weed_Killer"] = 5.00;
+
+
 
 $Farming::LotRotatePrice = 20;
 $Farming::LotUnloadPrice = 200;
