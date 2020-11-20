3b5520a415ff5e53b0c4cc17a59b961dfca477c5
ConanODoyle <this.is.not.a.email.account@gmail.com>
2020-11-20 10:06:50 -0800

    Adjustments and improvements to crop info dialogue

--- scripts/botDialogues/cropInfo.cs
+++ scripts/botDialogues/cropInfo.cs
@@ -11,7 +11,7 @@ $obj = new ScriptObject(CropInfoStart)
 	messageCount = 2;
 	message[0] = "Hello!";
 	messageTimeout[0] = 1;
-	message[1] = "I can tell you information about any crop!";
+	message[1] = "I've got info on every crop!";
 	messageTimeout[1] = 2;
 
 	dialogueTransitionOnTimeout = "CropInfoCore";
@@ -26,7 +26,7 @@ $obj = new ScriptObject(CropInfoCore)
 	response["Error"] = "ErrorResponse";
 
 	messageCount = 1;
-	message[0] = "What crop would you like to know more about?";
+	message[0] = "Which crop would you like to know more about? Say which crop's name.";
 	messageTimeout[0] = 2;
 
 	waitForResponse = 1;
@@ -70,6 +70,11 @@ $CropInfoSet.add($obj);
 function cropInfoSelectionParser(%dataObj, %msg)
 {
 	%msg = trim(strLwr(%msg));
+	%msg = strReplace(%msg, "cacti", "cactus");
+	%msg = strReplace(%msg, "daisies", "daisy");
+	%msg = strReplace(%msg, "lilies", "lily");
+	%msg = strReplace(%msg, "blueberries", "blueberry");
+
 	if (strLen(%msg) < 3)
 	{
 		return "Error";
@@ -109,58 +114,61 @@ function setupCropInfo(%dataObj)
 	switch$ (%crop)
 	{
 		case "Potato": 
-			%s1 = "Potatoes are very quick to grow, are harvested once, and drop between 2-4 potatoes and usually a seed.";
-			%s2 = "Be careful not to let them grow too long or else they'll drop less!";
+			%s1 = "Potatoes are very quick to grow, and drop between 2-4 potatoes and usually a seed.";
+			%s2 = "Be careful not to let them grow too long or else they'll give less! Keep an eye on how many potatoes are showing.";
 		case "Carrot":
-			%s1 = "Carrots are quick to grow, are harvested once, and drop between 2-3 carrots and usually a seed.";
-			%s2 = "If they grow too long, they'll drop less, so keep an eye on them!";
+			%s1 = "Carrots are quick to grow, and give between 2-3 carrots and usually a seed. They sell for slightly more than potatoes!";
+			%s2 = "Like potatoes, if they grow too long, they'll drop less, so keep an eye on them!";
 		case "Onion":
-			%s1 = "Onions take a while to grow, but give a lot of experience and can be carried in big stacks. They drop 3-5 onions.";
-			%s2 = "You can harvest them early, but you'll get fewer onions and a lower chance for a seed drop.";
+			%s1 = "Onions take a while to grow, but give a lot of experience and are carried in big stacks. Each plant drops 3-5 onions and often a seed.";
+			%s2 = "You can harvest them early and still get full experience, but you'll get fewer onions and a lower chance for a seed.";
 		case "Turnip":
-			%s1 = "Turnips are profitable cash crops, but take a decent bit to grow, and don't stack well.";
-			%s2 = "I hear sometimes they can look like they're staring at you...";
+			%s1 = "Turnips are profitable cash crops, but take a decent bit to grow, and don't stack well. Use a trowel to boost their yield!";
+			%s2 = "I hear sometimes they look like they're staring at you...";
 		case "Portobello":
-			%s1 = "Portobellos are mushrooms that are sensitive to light. They even grow with no light, but need nutrients to grow.";
-			%s2 = "However, they still want some light - wrong lighting will slow their growth. Nutrients will speed it up.";
+			%s1 = "Portobellos are mushrooms sensitive to light. They can even grow with no light, but need nutrients to grow.";
+			%s2 = "However, they still want some light - anything different than what they want will slow growth. Use an organic analyzer to check!";
 
 		case "Tomato":
-			%s1 = "Tomatoes are cheap crops that can be harvested up to 12 times. They drop 2-5 per harvest.";
+			%s1 = "Tomatoes are cheap crops that can be harvested 12 times. They drop 2-5 tomatoes per harvest and take relatively little water.";
 			%s2 = "They cost 10 experience to plant, and give 0-1 experience per harvest.";
 		case "Corn":
-			%s1 = "Corn can be harvested up to 3 times and drop 3-4 per harvest, but need nutrients to reach their final stage.";
-			%s2 = "They cost 9 experience to plant, and give 2-3 experience per harvest.";
+			%s1 = "Corn can be harvested 3 times, and drop 3-4 per harvest, but need nutrients to finish growing. They can be harvested earlier for less yield.";
+			%s2 = "They cost 9 experience to plant, and give 2-3 experience per harvest. Use an organic analyzer to figure out what nutrients they need!";
 		case "Wheat":
-			%s1 = "Wheat is a more compact alternative to corn, dropping 3-5 per harvest. They take slightly longer to grow as well.";
-			%s2 = "They cost 12 experience to plant, and give 3-4 experience per harvest. It also needs nutrients to grow fully.";
+			%s1 = "Wheat is a more compact alternative to corn, dropping 3-5 per harvest. They take slightly longer to grow as well, but use less nutrients.";
+			%s2 = "They cost 12 experience to plant, and give 3-4 experience per harvest. Use an organic analyzer to figure out what nutrients they need!";
 		case "Cabbage":
-			%s1 = "Cabbage is a water hungry crop, and drops 2-3 per harvest. Unlike corn and wheat, they don't need nutrients.";
-			%s2 = "You can harvest them 4 times, returning 3-5 exp per harvest. They cost 20 experience to plant.";
+			%s1 = "Cabbage is a water-hungry crop, and drops 2-3 per harvest. However, they don't need nutrients to finish growing!";
+			%s2 = "You can harvest them 4 times, giving 3-5 experience per harvest. They cost 20 experience to plant.";
 		case "Blueberry":
-			%s1 = "Blueberries are the alternative version of turnips, able to be harvested 3 times, for 2-6 berries per harvest.";
-			%s2 = "However, they need nutrients to grow fully. They cost 20 experience to plant, and give 5-6 per harvest.";
+			%s1 = "Blueberries are profitable cash crops, able to be harvested 3 times for 2-6 berries per harvest.";
+			%s2 = "However, they need nutrients to grow fully. They cost 20 experience to plant, and give 5-6 experience per harvest.";
 		case "Chili":
-			%s1 = "Chilis are a desert crop, using very little water. They need nutrients to fully grow, and drop 2-5 per harvest.";
-			%s2 = "They can be harvested 2 times, costing 12 experience and returning 5-6 per harvest.";
+			%s1 = "Chilis are a desert crop that use very little water and drop 2-5 per harvest. However, they need nutrients to fully grow.";
+			%s2 = "They can be harvested 2 times, costing 12 experience and returning 5-6 experience per harvest.";
 		case "Watermelon":
-			%s1 = "Watermelon are an unusual desert crop, not wanting any water at all in their second-to-last stage.";
-			%s2 = "They drop 1-2 watermelons every harvest, and in total get 8 harvests. They cost 40 exp to plant, and give 3-5.";
+			%s1 = "Watermelon are an unusual desert crop, which grows faster without any water at all in their second-to-last stage.";
+			%s2 = "They drop 1-2 watermelons every harvest, and in total get 8 harvests. They cost 40 experience to plant, and give 3-5 experience per harvest.";
 
 		case "Cactus":
-			%s1 = "Cacti are the smallest tree-like plant, able to grow with no water at all. Each harvest gives 2-5 fruit.";
+			%s1 = "Cacti are small tree-like plants, able to grow with no water at all (but grow faster with). Each harvest gives 2-5 cactus fruit.";
+			%s2 = "They can be harvested up to 15 times, giving 3-5 experience per harvest. They cost 60 experience to plant.";
+		case "Cacti":
+			%s1 = "Cacti are small tree-like plants, able to grow with no water at all (but grow faster with). Each harvest gives 2-5 cactus fruit.";
 			%s2 = "They can be harvested up to 15 times, giving 3-5 experience per harvest. They cost 60 experience to plant.";
 		case "Apple":
-			%s1 = "Apples have the smallest radius and do not shade plants under it. Like all trees, it needs nutrients to grow.";
-			%s2 = "They can be harvested up to 35 times for 8-14 apples, and can be pruned during its harvesting loop for more yield.";
+			%s1 = "Apples have the smallest plant radius and hardly shade any plants under it. Like all trees, it needs nutrients to grow.";
+			%s2 = "They can be harvested up to 35 times for 8-14 apples, and can be pruned during its flower or pre-flower stage for around 4 more apples.";
 		case "Mango":
-			%s1 = "Mangoes cover a wide area and create significant shade, which reduces some plants' growth time.";
+			%s1 = "Mangoes cover a wide area and cast significant shade on crops underneath, which can reduce plants' growth time.";
 			%s2 = "They can be harvested up to 35 times, dropping 11-16 per harvest.";
 		case "Peach":
-			%s1 = "Peach trees require less water than other trees, but need more nutrients to produce a harvest.";
+			%s1 = "Peach trees require less water than other trees, but need 3x more nutrients to reach its harvesting stage.";
 			%s2 = "They can be harvested up to 50 times, dropping 6-12 per harvest.";
 		case "Date":
-			%s1 = "Date trees require no water to grow, and in fact grow significantly slower if water is present.";
-			%s2 = "Like peaches, they can be harvested up to 50 times, dropping 6-12 per harvest.";
+			%s1 = "Date trees require no water to grow, and in fact grow significantly slower if water is present. It needs the same amount of nutrients as peaches do.";
+			%s2 = "Just like peaches, they can be harvested up to 50 times, dropping 6-12 per harvest.";
 
 		case "Lily":
 			%s1 = "Lilies are flowers that add 2 nitrogen to soil every minute, in their final flowering stage.";
