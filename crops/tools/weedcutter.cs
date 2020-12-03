5c4c5a2b12fcf9cabab32009f29164c4139b8dcf
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-02 21:05:04 -0800

    Fix weedcutter durability display issues

--- crops/tools/weedcutter.cs
+++ crops/tools/weedcutter.cs
@@ -17,7 +17,7 @@ datablock ItemData(WeedCutterItem : HammerItem)
 	bonusDurability = 10;
 };
 
-datablock ShapeBaseImageData(WeedCutterImage)
+datablock ShapeBaseImageData(WeedCutterImage : TrowelImage)
 {
 	shapeFile = "./weedWhacker/weed_whacker.dts";
 
@@ -32,26 +32,12 @@ datablock ShapeBaseImageData(WeedCutterImage)
 	stateTimeoutValue[2] = 0.4;
 
 	toolTip = "Area remove weeds";
-
-	stateName[0] = "Activate";
-	stateTimeoutValue[0] = 0.1;
-	stateTransitionOnTimeout[0] = "Ready";
-
-	stateName[1] = "Ready";
-	stateTransitionOnTriggerDown[1] = "Fire";
-
-	stateName[2] = "Fire";
-	stateScript[2] = "onFire";
-	stateTransitionOnTriggerUp[2] = "Ready";
-	stateTimeoutValue[2] = 0.2;
-	stateTransitionOnTimeout[2] = "Repeat";
-	stateWaitForTimeout[2] = 1;
-
-	stateName[3] = "Repeat";
-	stateTimeoutValue[3] = 0.12;
-	stateTransitixonOnTimeout[3] = "Fire";
-	stateTransitionOnTriggerUp[3] = "Ready";
 };
+
+function WeedCutterImage::onReady(%this, %obj, %slot)
+{
+	harvestToolReady(%this, %obj, %slot);
+}
 
 function WeedCutterImage::onFire(%this, %obj, %slot)
 {
