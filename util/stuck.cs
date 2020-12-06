e450f3c6c3123270b89bebf8f647129d1f20a7e8
Pecon <admin@pecon.us>
2020-12-06 10:52:56 -0800

    Majorly improve /stuck

--- util/stuck.cs
+++ util/stuck.cs
@@ -4,8 +4,28 @@ function serverCmdStuck(%cl)
 	{
 		if (isObject(%pl = %cl.player))
 		{
-			%pl.setTransform(vectorAdd(%pl.getTransform(), "0 0 4") SPC getWords(%pl.getTransform(), 3, 6));
-			%cl.nextUnstuckTime = $Sim::Time + 3;
+			%start = vectorAdd(%pl.getPosition(), "0 0 3");
+			%end = %pl.getPosition();
+			%ray = containerRaycast(%start, %end, $TypeMasks::FxBrickObjectType | $TypeMasks::VehicleObjectType | $TypeMasks::PlayerObjectType, %pl);
+
+			if(isObject(firstWord(%ray)))
+			{
+				%newPos = getWords(%ray, 1, 3);
+				%rotation = getWords(%pl.getTransform(), 3, 6);
+				%normal = getWords(%ray, 4, 7);
+
+				// Adjust for sloped surfaces
+				%dot = 1 - vectorDot("0 0 1", %normal);
+				%newPos = vectorAdd(%newPos, "0 0" SPC %dot);
+
+				%pl.setTransform(%newPos SPC %rotation);
+				%cl.nextUnstuckTime = $Sim::Time + 3;
+			}
+			else
+			{
+				%cl.nextUnstuckTime = $Sim::Time + 1;
+				%cl.chatMessage("You are not stuck.");
+			}
 		}
 	}
 	else
