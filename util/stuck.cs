f52291ffdfee8670752385576fb133e61a330e31
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-08 14:21:01 -0800

    Improve /stuck object detection

--- util/stuck.cs
+++ util/stuck.cs
@@ -4,28 +4,34 @@ function serverCmdStuck(%cl)
 	{
 		if (isObject(%pl = %cl.player))
 		{
-			%start = vectorAdd(%pl.getPosition(), "0 0 3");
-			%end = %pl.getPosition();
-			%ray = containerRaycast(%start, %end, $TypeMasks::FxBrickObjectType | $TypeMasks::VehicleObjectType | $TypeMasks::PlayerObjectType, %pl);
-
-			if(isObject(firstWord(%ray)))
+			%xdiffs = "0.5 0.5 -0.5 -0.5 0";
+			%ydiffs = "0.5 -0.5 0.5 -0.5 0";
+			for (%i = 0; %i < getWordCount(%xdiffs); %i++)
 			{
-				%newPos = getWords(%ray, 1, 3);
-				%rotation = getWords(%pl.getTransform(), 3, 6);
-				%normal = getWords(%ray, 4, 7);
+				%offset = getWord(%xdiffs, %i) SPC getWord(%ydiffs, %i) SPC 3;
+				%start = vectorAdd(%pl.getPosition(), %offset);
+				%end = vectorAdd(%start, "0 0 -3");
+				%ray = containerRaycast(%start, %end, $TypeMasks::FxBrickObjectType | $TypeMasks::VehicleObjectType | $TypeMasks::PlayerObjectType, %pl);
 
-				// Adjust for sloped surfaces
-				%dot = 1 - vectorDot("0 0 1", %normal);
-				%newPos = vectorAdd(%newPos, "0 0" SPC %dot);
+				if(isObject(firstWord(%ray)))
+				{
+					%newPos = getWords(%ray, 1, 3);
+					%rotation = getWords(%pl.getTransform(), 3, 6);
+					%normal = getWords(%ray, 4, 7);
 
-				%pl.setTransform(%newPos SPC %rotation);
-				%cl.nextUnstuckTime = $Sim::Time + 3;
+					// Adjust for sloped surfaces
+					%dot = 1 - vectorDot("0 0 1", %normal);
+					%newPos = vectorAdd(%newPos, "0 0" SPC %dot);
+
+					%pl.setTransform(%newPos SPC %rotation);
+					%cl.nextUnstuckTime = $Sim::Time + 3;
+					return;
+				}
 			}
-			else
-			{
-				%cl.nextUnstuckTime = $Sim::Time + 1;
-				%cl.chatMessage("You are not stuck.");
-			}
+
+			//did not find anything
+			%cl.nextUnstuckTime = $Sim::Time + 1;
+			%cl.chatMessage("You are not stuck.");
 		}
 	}
 	else
