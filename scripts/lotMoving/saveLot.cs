01a02e275cf81aa0dacb8b41b88da18628c62197
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-08 21:38:06 -0800

    Continue work on save rewrite

--- scripts/lotMoving/saveLot.cs
+++ scripts/lotMoving/saveLot.cs
@@ -461,6 +461,237 @@ function farmingSaveLot(%bl_id, %delete)
 
 
 
+$Pref::Farming::SaveLot::MaxBricksPerTick = 128;
+
+
+// setup:
+//	farmingSaveLot(%bl_id, %delete) - does basic checks/init, starts search for bricks
+//	=> farmingSaveCollectBricks(%brickgroup) - starts recursive brick search loop
+//	==> farmingSaveRecursiveCollectBricks(%col, %q, %vis) - runs loop until all bricks found
+//	=> farmingSaveWriteSave(%col) - writes save given collection object
+
+
+
+function farmingSaveInitFile(%bl_id, %type)
+{
+	%date = getDateTime();
+	%save_date = strReplace(getWord(%date, 0), "/", "-");
+	%save_time = stripChars(getWord(%date, 1), ":");
+
+	%path = $Pref::Server::AS_["Directory"] @ %type @ "s/" @ %bl_id @ "/" @ %type @ " Autosave (" @ %bl_id @ ") - " @ %save_date @ " at " @ %save_time @ ".bls";
+	%file = new FileObject();
+	%file.savingBL_ID = %bl_id;
+	%file.savingGroup = "BrickGroup_" @ %bl_id;
+	%file.path = %path;
+
+	if (!isObject(%file.savingGroup))
+	{
+		%file.delete();
+		return 0;
+	}
+
+	%file.openForWrite(%path);
+	%file.writeLine("This is a Farming " @ strLwr(%type) @ " autosave! Don't modify it, you'll likely cause it to load improperly.");
+	%file.writeLine("1");
+	%file.writeLine("0 0 0"); // offset is 0
+
+	for(%i = 0; %i < 64; %i++)
+	{
+		%color = getColorIDTable(%i);
+		%file.writeLine(%color);
+	}
+
+	return %file;
+}
+
+
+function farmingSaveWriteBrick(%file, %brick)
+{
+	%brickData = %brick.getDataBlock();
+	%uiName = %brickData.uiName;
+	%trans = %brick.getTransform();
+	%pos = vectorSub(getWords(%trans, 0, 2), %file.lotOrigin);
+	if (%brickData.hasPrint)
+	{
+		%filename = getPrintTexture(%brick.getPrintID());
+		%fileBase = fileBase(%filename);
+		%path = filePath(%filename);
+		if (%path $= "" || %filename $= "base/data/shapes/bricks/brickTop.png")
+		{
+			%printTexture = "/";
+		}
+		else
+		{
+			%dirName = getSubStr(%path, strlen("Add-Ons/"), strlen(%path) - strlen("Add-Ons/"));
+			%posA = strpos(%dirName, "_");
+			%posB = strpos(%dirName, "_", %posA + 1);
+			%aspectRatio = getSubStr(%dirName, %posA + 1, (%posB - %posA) - 1);
+			%printTexture = %aspectRatio @ "/" @ %fileBase;
+		}
+	}
+	else
+	{
+		%printTexture = "";
+	}
+
+	//use isLot/isShopLot for determining isBaseplate field
+	%isLot = %brickData.isLot || %brickData.isShopLot;
+	%line = %uiName @ "\"" SPC %pos SPC %brick.getAngleID() SPC %isLot SPC %brick.getColorID() SPC %printTexture SPC %brick.getColorFxID() SPC %brick.getShapeFxID() SPC %brick.isRayCasting() SPC %brick.isColliding() SPC %brick.isRendering();
+	%file.writeLine(%line);
+
+	//always write ownership
+	%line = "+-OWNER" SPC %brick.getGroup().bl_id;
+	%file.writeLine(%line);
+
+	if (%brick.getName() !$= "")
+	{
+		%line = "+-NTOBJECTNAME" SPC %brick.getName();
+		%file.writeLine(%line);
+	}
+
+	for (%i = 0; %i < %brick.numEvents; %i++)
+	{
+		%class = %brick.getClassName();
+		%enabled = %brick.eventEnabled[%i];
+		%inputName = $InputEvent_Name[%class, %brick.eventInputIdx[%i]];
+		%delay = %brick.eventDelay[%i];
+		if (%brick.eventTargetIdx[%i] == -1)
+		{
+			%targetName = -1;
+			%NT = %brick.eventNT[%i];
+			%targetClass = "FxDTSBrick";
+		}
+		else
+		{
+			%targetList = $InputEvent_TargetList[%class, %brick.eventInputIdx[%i]];
+			%target = getField(%targetList, %brick.eventTargetIdx[%i]);
+			%targetName = getWord(%target, 0);
+			%targetClass = getWord(%target, 1);
+			%NT = "";
+		}
+		%outputName = $OutputEvent_Name[%targetClass, %brick.eventOutputIdx[%i]];
+		%line = "+-EVENT" TAB %i TAB %enabled TAB %inputName TAB %delay TAB %targetName TAB %NT TAB %outputName;
+		%par1 = %brick.eventOutputParameter[%i, 1];
+		%par2 = %brick.eventOutputParameter[%i, 2];
+		%par3 = %brick.eventOutputParameter[%i, 3];
+		%par4 = %brick.eventOutputParameter[%i, 4];
+		%line = %line TAB %par1 TAB %par2 TAB %par3 TAB %par4;
+		%file.writeLine(%line);
+	}
+
+	if (isObject(%brick.emitter))
+	{
+		%line = "+-EMITTER" SPC %brick.emitter.getEmitterDataBlock().uiName @ "\" " @ %brick.emitterDirection;
+		%file.writeLine(%line);
+	}
+	else if (%brick.emitterDirection != 0)
+	{
+		%line = "+-EMITTER NONE\" " @ %brick.emitterDirection;
+		%file.writeLine(%line);
+	}
+
+	if (isObject(%brick.light))
+	{
+		%line = "+-LIGHT" SPC %brick.light.getDataBlock().uiName @ "\"" SPC %brick.light.Enable;
+		%file.writeLine(%line);
+	}
+
+	if (isObject(%brick.Item))
+	{
+		%line = "+-ITEM" SPC %brick.Item.getDataBlock().uiName @ "\" " @ %brick.itemPosition SPC %brick.itemDirection SPC %brick.itemRespawnTime;
+		%file.writeLine(%line);
+	}
+
+	else if ((%brick.itemDirection != 2 && %brick.itemDirection !$= "") || %brick.itemPosition != 0 || (%brick.itemRespawnTime != 0 && %brick.itemRespawnTime != 4000))
+	{
+		%line = "+-ITEM NONE\" " @ %brick.itemPosition SPC %brick.itemDirection SPC %brick.itemRespawnTime;
+		%file.writeLine(%line);
+	}
+
+	if (isObject(%brick.AudioEmitter))
+	{
+		%line = "+-AUDIOEMITTER" SPC %brick.AudioEmitter.getProfileId().uiName @ "\" ";
+		%file.writeLine(%line);
+	}
+
+	if (isObject(%brick.VehicleSpawnMarker))
+	{
+		%line = "+-VEHICLE" SPC %brick.VehicleSpawnMarker.getUiName() @ "\" " @ %brick.VehicleSpawnMarker.getReColorVehicle();
+		%file.writeLine(%line);
+	}
+}
+
+function farmingSaveCollectBricks(%group, %lotlist)
+{
+	if (!isObject(%group))
+	{
+		return;
+	}
+
+	%collection = new SimSet();
+	%queue = new SimSet();
+	%visited = new SimSet();
+
+	%group.refreshLotList();
+
+	%collection.bl_id = %group.bl_id;
+	%queue.lotList = %lotList;
+
+	farmingSaveRecursiveCollectBricks(%collection, %queue, %visited);
+}
+
+function farmingSaveRecursiveCollectBricks(%collection, %queue, %visited)
+{
+	if (getWordCount(%queue.lotList) <= 0)
+	{
+		%queue.clear();
+		%visited.clear();
+		%queue.delete();
+		%visited.delete();
+
+		farmingSaveWriteSave(%collection);
+		return;
+	}
+
+	%lot = getWord(%queue.lotList, 0);
+	%queue.lotList = getWords(%queue.lotList, 1, 20);
+
+	if (isObject(%lot))
+	{
+		recursiveGatherBricks(%collection, %queue, %visited, "farmingSaveRecursiveCollectBricks");
+	}
+	else
+	{
+		farmingSaveRecursiveCollectBricks(%collection, %queue, %visited);
+	}
+}
+
+function farmingSaveWriteSave(%collection)
+{
+	%center = %collection.singleLots;
+	if (getWordCount(%center) > 1)
+	{
+		talk("ERROR: farmingSaveWriteSave - multiple single lots found! " @ %center);
+		error("ERROR: farmingSaveWriteSave - multiple single lots found! " @ %center);
+
+		%collection.clear();
+		%collection.delete();
+		return;
+	}
+
+	%file = farmingSaveInitFile(%collection.bl_id)
+}
+
+function recursiveFarmingWriteSave(%collection)
+{
+
+}
+
+
+
+
+
+
 
 // external: (call with these parameters)
 // 	%collection: SimSet - holds all found bricks
@@ -471,6 +702,7 @@ function farmingSaveLot(%bl_id, %delete)
 //	%rootBrick: current brick we're looking at
 //	%index: current index of the upbricks we're on
 //	%searchDown: searching downwards?
+// skips public lotbricks
 function recursiveGatherBricks(%collection, %queue, %visited, %callback, %rootBrick, %index, %searchDown)
 {
 	if (!isObject(%rootBrick) && %queue.getCount() <= 0) //base case, exit
@@ -498,58 +730,50 @@ function recursiveGatherBricks(%collection, %queue, %visited, %callback, %rootBr
 
 	//i hate complex for loops cause the more complex the logic is the harder it is to be sure the logic is perfectly sound
 	//for example, accidentally skipping the first brick due to off by one errors
-	if (!%searchDown) //searching upwards
+	if (!%searchDown)
 	{
-		%upBrickCount = %rootBrick.getNumUpBricks();
-		for (%i = 0; %i < $Pref::Farming::SaveLot::MaxBricksPerTick; %i++)
-		{
-			if (%index >= %upBrickCount)
-			{
-				%index = 0;
-				%searchDown = 1; //prepare for searching downwards
-				break;
-			}
-			%next = %rootBrick.getUpBrick(%index);
-
-			if (!%collection.isMember(%next)) //add all bricks, including public if for some reason player bricks are above public ones
-			{
-				%collection.add(%next);
-			}
-			if (!%visited.isMember(%next) && !%queue.isMember(%next)) //add non-visited, non-present bricks to queue
-			{
-				%queue.add(%next);
-			}
-
-			%index++;
-		}
+		%func = "getUpBrick";
+		%count = %rootBrick.getNumUpBricks();
 	}
-	else //searching downwards
+	else
 	{
-		%downBrickCount = %rootBrick.getNumDownBricks();
-		for (%i = 0; %i < $Pref::Farming::SaveLot::MaxBricksPerTick; %i++)
-		{
-			if (%index >= %downBrickCount)
-			{
-				%doneSearching = 1;
-				%searchDown = 0; //we're done
-				break;
-			}
-			%next = %rootBrick.getDownBrick(%index);
-
-			if (!%collection.isMember(%next)) //add all bricks, including public if for some reason player bricks are above public ones
-			{
-				%collection.add(%next);
-			}
-			if (!%visited.isMember(%next) && !%queue.isMember(%next)) //add non-visited, non-present bricks to queue
-			{
-				%queue.add(%next);
-			}
-
-			%index++;
-		}
+		%func = "getDownBrick";
+		%count = %rootBrick.getNumDownBricks();
 	}
 
-	if (%doneSearching)
+	for (%i = 0; %i < $Pref::Farming::SaveLot::MaxBricksPerTick; %i++)
+	{
+		if (%index >= %count)
+		{
+			%index = 0;
+			%searchDown++;
+			break;
+		}
+		%next = call(%rootBrick, %func, %index);
+
+		//skip checking/adding public non-lot bricks
+		if (%next.getGroup().bl_id == 888888 && !(%next.getDatablock().isLot || %next.getDatablock().isShopLot))
+		{
+			continue;
+		}
+
+		if (!%collection.isMember(%next)) //add all bricks, including public if for some reason player bricks are above public ones
+		{
+			%collection.add(%next);
+			if (%next.getDatablock().isSingle)
+			{
+				%collection.singleLots = trim(%collection.singleLots SPC %next);
+			}
+		}
+		if (!%visited.isMember(%next) && !%queue.isMember(%next)) //add non-visited, non-present bricks to queue
+		{
+			%queue.add(%next);
+		}
+
+		%index++;
+	}
+
+	if (%searchDown >= 2) //done searching up (0) and down (1)
 	{
 		schedule(3, MissionCleanup, recursiveGatherBricks, %collection, %queue, %visited, %callback, 0, 0, 0);
 	}
