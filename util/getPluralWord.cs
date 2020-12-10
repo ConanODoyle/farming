f36ff2455d2693ff806ece2c7fcfe7b79e57b97f
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-09 17:24:06 -0800

    Fix some plural words

--- util/getPluralWord.cs
+++ util/getPluralWord.cs
@@ -1,28 +1,32 @@
 function getPluralWord(%word)
 {
-	%lastChar = getSubStr(%word, getMax(0, strLen(%word) - 1), 1);
-	%lastWord = getWord(%word, getWordCount(%word) - 1);
-	%noPluralAppend = "wheat corn coal phosphate sticks";
-	%esAppend = "tomato potato";
-	
-	if (strPos(%noPluralAppend, strLwr(%lastWord)) >= 0)
-	{
-		return %word;
-	}
-	else if (%lastChar $= "y")
-	{
-		return getSubStr(%word, 0, strLen(%word) - 1) @ "ies";
-	}
-	else if (%lastChar $= "x" || %lastChar $= "s" || strPos(%esAppend, strLwr(%lastWord)) >= 0)
-	{
-		return %word @ "es";
-	}
-	else if (%lastWord $= "Cactus")
-	{
-		return getSubStr(%word, 0, strLen(%word) - 2) @ "i";
-	}
-	else
-	{
-		return %word @ "s";
-	}
+    %lastChar = getSubStr(%word, getMax(0, strLen(%word) - 1), 1);
+    %lastWord = getWord(%word, getWordCount(%word) - 1);
+    %noPluralAppend = "wheat corn coal phosphate sticks";
+    %esAppend = "tomato potato peach gameboy";
+    
+    if (strPos(%noPluralAppend, strLwr(%lastWord)) >= 0)
+    {
+        return %word;
+    }
+    else if (strPos(%esAppend, strLwr(%lastWord)) >= 0) //prioritize custom cases
+    {
+        return %word @ "es";
+    }
+    else if (%lastChar $= "y")
+    {
+        return getSubStr(%word, 0, strLen(%word) - 1) @ "ies";
+    }
+    else if (%lastChar $= "x" || %lastChar $= "s")
+    {
+        return %word @ "es";
+    }
+    else if (%lastWord $= "Cactus")
+    {
+        return getSubStr(%word, 0, strLen(%word) - 2) @ "i";
+    }
+    else
+    {
+        return %word @ "s";
+    }
 }
