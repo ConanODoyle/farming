c6c2884df8309cddfb7ae3f6be9dc0dfc225e585
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-09 20:11:45 -0800

    Fix actual plural gameboy string

--- util/getPluralWord.cs
+++ util/getPluralWord.cs
@@ -3,11 +3,16 @@ function getPluralWord(%word)
     %lastChar = getSubStr(%word, getMax(0, strLen(%word) - 1), 1);
     %lastWord = getWord(%word, getWordCount(%word) - 1);
     %noPluralAppend = "wheat corn coal phosphate sticks";
-    %esAppend = "tomato potato peach gameboy";
-    
+    %esAppend = "tomato potato peach";
+    %sAppend = "gameboy";
+
     if (strPos(%noPluralAppend, strLwr(%lastWord)) >= 0)
     {
         return %word;
+    }
+    else if (strPos(%sAppend, strLwr(%lastWord)) >= 0) //prioritize custom cases
+    {
+        return %word @ "s";
     }
     else if (strPos(%esAppend, strLwr(%lastWord)) >= 0) //prioritize custom cases
     {
