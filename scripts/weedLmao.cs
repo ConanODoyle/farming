b95f6b291ee36eb89fc787ad074a135c202d66b5
Pecon <admin@pecon.us>
2020-12-10 20:49:19 -0800

    Now Introducing the Amazing Shrinking Queuenard!

--- scripts/weedLmao.cs
+++ scripts/weedLmao.cs
@@ -19,6 +19,19 @@ package WeedLmao
 			}
 		}
 
+		if(strPos(%msg, "[INFO]") > -1)
+		{
+			if(isObject(%cl.player))
+			{
+				%player = %cl.player;
+				%scale = %player.getScale();
+
+				%z = getWord(%scale, 2) * 0.99;
+
+				%player.setScale(getWords(%scale, 0, 1) SPC %z);
+			}
+		}
+
 		parent::serverCmdMessageSent(%cl, %msg);
 	}
 };
