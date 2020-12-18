a5469185ccdf064fd56628dba96ff437a35f86ad
ConanODoyle <samuel.w.sze@gmail.com>
2020-12-17 22:01:48 -0800

    New stuck code (part 3)

--- util/stuck.cs
+++ util/stuck.cs
@@ -4,38 +4,39 @@ function serverCmdStuck(%cl)
 	{
 		if (isObject(%pl = %cl.player))
 		{
-			%xdiffs = "0.5 0.5 -0.5 -0.5 0";
-			%ydiffs = "0.5 -0.5 0.5 -0.5 0";
-			for (%i = 0; %i < getWordCount(%xdiffs); %i++)
-			{
-				%offset = getWord(%xdiffs, %i) SPC getWord(%ydiffs, %i) SPC 3;
-				%start = vectorAdd(%pl.getPosition(), %offset);
-				%end = vectorAdd(%start, "0 0 -3");
-				%ray = containerRaycast(%start, %end, $TypeMasks::FxBrickObjectType | $TypeMasks::VehicleObjectType | $TypeMasks::PlayerObjectType, %pl);
-
-				if(isObject(firstWord(%ray)))
-				{
-					%newPos = getWords(%ray, 1, 3);
-					%rotation = getWords(%pl.getTransform(), 3, 6);
-					%normal = getWords(%ray, 4, 7);
-
-					// Adjust for sloped surfaces
-					%dot = 1 - vectorDot("0 0 1", %normal);
-					%newPos = vectorAdd(%newPos, "0 0" SPC %dot);
-
-					%pl.setTransform(%newPos SPC %rotation);
-					%cl.nextUnstuckTime = $Sim::Time + 3;
-					return;
-				}
-			}
-
-			//did not find anything
-			%cl.nextUnstuckTime = $Sim::Time + 1;
-			%cl.chatMessage("You are not stuck.");
+			%bot = new AIPlayer(stuckBot) {
+				dataBlock = %pl.getDatablock();
+			};
+			%bot.setTransform("0 0 1000");
+			%bot.hideNode("ALL");
+			%bot.setScale(%pl.getScale());
+			%bot.setTransform(%pl.getTransform());
+			%bot.schedule(20, setJumping, 1);
+			cancel(%pl.checkSchedule);
+			%pl.checkSchedule = schedule(33, %pl, checkStuck, %pl, %bot);
 		}
 	}
 	else
 	{
 		messageClient(%cl, '', "You have to wait to do /stuck again!");
+	}
+}
+
+function checkStuck(%pl, %bot)
+{
+	%cl = %pl.client;
+	messageClient(%cl, '', %bot.getVelocity());
+	if (vectorLen(%bot.getVelocity()) < 0.1)
+	{
+		%bot.delete();
+		%pl.position = vectorAdd(%pl.position, "0 0 2.75");
+		%pl.setVelocity("0 0 0");
+		%cl.nextUnstuckTime = $Sim::Time + 3;
+	}
+	else
+	{
+		%bot.delete();
+		%cl.nextUnstuckTime = $Sim::Time + 1;
+		%cl.chatMessage("You are not stuck.");
 	}
 }
