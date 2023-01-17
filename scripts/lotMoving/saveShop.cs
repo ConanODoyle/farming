eb34fd6ff52f877cf8a301ff11a9840bc3671002
irrelevantirreverent <irrelevant.irreverent@gmail.com>
2023-01-17 02:28:32 -0500

    Redirect shop lot saving to new functions
    Also

--- scripts/lotMoving/saveShop.cs
+++ scripts/lotMoving/saveShop.cs
@@ -1,29 +1,26 @@
 function farmingSaveShop(%bl_id, %delete)
 {
-	%brickGroup = "BrickGroup_" @ %bl_id;
-
-	if (!isObject(%brickGroup))
+	%bg = "brickGroup_" @ %bl_id;
+	if (!isObject(%bg))
 	{
-		error("ERROR: farmingSaveShop - Client has no brickgroup! " @ %client);
-		echo("ERROR: farmingSaveShop - Client has no brickgroup! " @ %client);
-		return -1;
+		error("ERROR: farmingSaveShop - brickGroup doesn't exist! " @ %bg);
+		return 0;
+	}
+	%collection = new SimSet();
+	%queue = new SimSet();
+	%visited = new SimSet();
+	if (%delete)
+	{
+		%collection.callbackOnComplete = "postSaveClearLot";
 	}
 
-	%lot = %brickGroup.shopLot;
+	%bg.refreshLotList();
 
-	if (!isObject(%lot))
-	{
-		error("ERROR: farmingSaveShop - Client's brickgroup doesn't have a shop lot!");
-		echo("ERROR: farmingSaveShop - Client's brickgroup doesn't have a shop lot!");
-		return -1;
-	}
+	%collection.bl_id = %bg.bl_id;
+	%collection.brickGroup = %bg;
+	%collection.lotList = %bg.lotList;
+	%collection.type = "Shop"; //required for farmingSaveWriteSave/farmingSaveInitFile
+	%queue.lotList = %bg.lotList;
 
-	%brickGroup.isSaveClearingLot = true;
-
-	%file = farmingSaveInitFile(%bl_id, "Shop");
-
-	$Farming::Temp::Origin[%file] = %lot.position;
-	$Farming::Temp::BrickQueue[%file] = new SimSet();
-	$Farming::Temp::BrickSet[%file] = new SimSet();
-	farmingSaveGatherBricks(%file, "Shop", %lot, %delete);
+	farmingSaveRecursiveCollectBricks(%collection, %queue, %visited);
 }
