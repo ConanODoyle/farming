f572e68d0928a5ba3852223634be3b8d722dfdfe
ConanODoyle <samuel.w.sze@gmail.com>
2023-01-18 19:14:07 -0500

    Add Bass Pro Shop Logo

---
+++ fishing/bassproshop/items.cs
@@ -0,0 +1,15 @@
+datablock ItemData(BassProSign1Item)
+{
+	shapeFile = "./BassProShopItem.dts";
+	uiName = "Bass Pro Shop";
+	doColorShift = false;
+	colorShiftColor = "1 1 1 1";
+};
+
+datablock ItemData(BassProSign2Item)
+{
+	shapeFile = "./BassProShopColorlessItem.dts";
+	uiName = "Bass Pro Shop Colorless";
+	doColorShift = false;
+	colorShiftColor = "1 1 1 1";
+};
