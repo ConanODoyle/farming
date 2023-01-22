4b1218301c1de37b95b4d0bbf7af3257cbff1bcc
Darksaber2213 <darksaber2213@outlook.com>
2023-01-21 22:51:47 -0700

    Added core exec file for the Harvester.
    Signed-off-by: Darksaber2213 <darksaber2213@outlook.com>

--- boss/exec.cs
+++ boss/exec.cs
@@ -1,1 +1,16 @@
+$Harvester::Root = filePath($Con::File);
+
 exec("./npc/dialogue.cs");
+
+exec("./classes/environment.cs");
+exec("./classes/HarvesterArmor.cs");
+exec("./classes/HarvesterBlade.cs");
+exec("./classes/HarvesterClusterBomb.cs");
+exec("./classes/HarvesterBeamRifle.cs");
+exec("./classes/HarvesterSpike.cs");
+exec("./classes/MasterKey.cs");
+exec("./classes/VoidKey.cs");
+
+exec("./scripts/ai.cs");
+exec("./scripts/cutscene.cs");
+exec("./scripts/fight.cs");
