6c76ae087ac1a0fb8c6ee570186e3286a25ed0e9
ConanODoyle <samuel.w.sze@gmail.com>
2023-02-25 15:36:15 -0500

    Add deghosting for bots

--- util/botPerformanceOverrides.cs
+++ util/botPerformanceOverrides.cs
@@ -10,6 +10,55 @@ BlockheadHoleBot.hWander = 0;
 BlockheadHoleBot.hStrafe = 0;
 BlockheadHoleBot.hGridWander = 0;
 
+if (!isObject(BotHoleSimSet))
+{
+	new SimSet(BotHoleSimSet);
+}
+
+function botHoleGhostLoop(%idx)
+{
+	cancel($BotHoleGhostLoopSchedule);
+	%idx = %idx + 0;
+
+	if (BotHoleSimSet.getCount() <= 0)
+	{
+		return;
+	}
+
+	//ghost/unghost up to 5 bots each tick
+	for (%i = 0; %i < 5; %i++)
+	{
+		if (%idx >= BotHoleSimSet.getCount())
+		{
+			break;
+		}
+
+		%bot = BotHoleSimSet.getObject(%idx);
+		for (%j = 0; %j < ClientGroup.getCount(); %j++)
+		{
+			%cl = ClientGroup.getObject(%j);
+			%pl = %cl.player;
+
+			if (!isObject(%pl) || vectorDist(%pl.position, %bot.position) > 150)
+			{
+				%bot.clearScopeToClient(%cl);
+			}
+			else
+			{
+				%bot.scopeToClient(%cl);
+			}
+		}
+
+		%idx++;
+	}
+
+	if (%idx >= BotHoleSimSet.getCount())
+	{
+		%idx = 0;
+	}
+
+	$BotHoleGhostLoopSchedule = schedule(33, BotHoleSimSet, botHoleGhostLoop, %idx);
+}
 
 function minDistanceToPlayer(%obj)
 {
@@ -61,7 +110,18 @@ package DespawnDistantBots
 			%brick.hSpawnDetectSchedule = %brick.scheduleNoQuota(5000, spawnHoleBot);
 			return 0;
 		}
-		return parent::spawnHoleBot(%brick);
+		%ret = parent::spawnHoleBot(%brick);
+
+		if (isObject(%bot = %brick.hBot) && !BotHoleSimSet.isMember(%brick.hBot))
+		{
+			BotHoleSimSet.add(%bot);
+			%bot.setNetFlag(6, true);
+			if (GhostAlwaysSet.isMember(%bot))
+				%bot.clearScopeAlways();
+
+			botHoleGhostLoop();
+		}
+		return %ret;
 	}
 };
 activatePackage(DespawnDistantBots);
