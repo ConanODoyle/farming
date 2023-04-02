016d6c1b0cd3e9d708b4772e5bf331609f76a6d8
ConanODoyle <samuel.w.sze@gmail.com>
2023-04-02 16:55:19 -0400

    Add new donor

--- scripts/donator.cs
+++ scripts/donator.cs
@@ -66,6 +66,7 @@ $isDonator_["6052"]		= 1 TAB "Sauce";
 $isDonator_["166053"]	= 1 TAB "Gentle Spy";
 $isDonator_["6835"]		= 1 TAB "SadBlobFish";
 $isDonator_["41812"]	= 1 TAB "meesh";
+$isDonator_["262122"]	= 1 TAB "[Fridge Raider]";
 
 $isBetaTester_["30881"]		= 1 TAB "Allun Pentax";
 $isBetaTester_["39617"]		= 1 TAB "Queuenard";
