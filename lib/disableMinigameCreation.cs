935a07c103eb2ad372f959b6decc7a978b996d53
ConanODoyle <samuel.w.sze@gmail.com>
2024-08-09 07:02:37 -0400

    Remove unneeded getMinigameFromObject which apparently has time impact

--- lib/disableMinigameCreation.cs
+++ lib/disableMinigameCreation.cs
@@ -1,3 +1,4 @@
 //just outright remove the function, fuck it
 
 schedule(1000, 0, eval, "function serverCmdCreateMinigame() {}");
+schedule(1000, 0, eval, "function getMiniGameFromObject(%obj) { return -1; }");
