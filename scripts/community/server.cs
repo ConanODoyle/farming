47bc07bdc8109b41e78bdcb72c81d40e4148ede0
Samuel Sze <>
2024-08-12 16:57:20 -0400

    Add framework for dailies and weeklies

---
+++ scripts/community/server.cs
@@ -0,0 +1,5 @@
+//General design:
+
+//Daily challenge: some selection of crops: deposit past a threshold to win bux and tix
+//Weekly challenge: smattering of crops: deposit past a threshold to win farmicoin
+
