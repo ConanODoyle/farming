7028dec43d3d07cbb3ad147e3150fa2aa478373b
ConanODoyle <>
2024-08-13 02:28:06 -0400

    Move long message box functions into its own support file

--- scripts/core/tutorial.cs
+++ scripts/core/tutorial.cs
@@ -28,46 +28,6 @@ registerInfo("Join the Discord here: <a:https://discord.gg/R3R3Vfj>https://disco
 
 
 //tutorial
-
-function gameConnection::messageBoxYesNoLong(%this, %title, %message, %taggedOkCmd)
-{
-	%maxSplits = 4;
-	%hardLimit = 255 * %maxSplits;
-
-	if(strLen(%message) > %hardLimit)
-	{
-		talk("ERROR: messageBoxYesNoLong max length breached!");
-		return;
-	}
-
-	for(%i = 0; %i < %maxSplits; %i++)
-	{
-		%splitString[%i] = getSubStr(%message, %i * 255, 255);
-	}
-
-	commandToClient(%this, 'messageBoxYesNo', %title, '%2%3%4%5', %taggedOkCmd, %splitString[0], %splitString[1], %splitString[2], %splitString[3]);
-}
-
-function gameConnection::messageBoxOKLong(%this, %title, %message, %taggedOkCmd)
-{
-	%maxSplits = 4;
-	%hardLimit = 255 * %maxSplits;
-
-	if(strLen(%message) > %hardLimit)
-	{
-		talk("ERROR: messageBoxOKLong max length breached!");
-		return;
-	}
-
-	for(%i = 0; %i < %maxSplits; %i++)
-	{
-		%splitString[%i] = getSubStr(%message, %i * 255, 255);
-	}
-
-	commandToClient(%this, 'messageBoxOK', %title, '%1%2%3%4', %splitString[0], %splitString[1], %splitString[2], %splitString[3]);
-}
-
-
 $header = "<font:Palatino Linotype:24>";
 $body = "<font:Arial:16>";
 
