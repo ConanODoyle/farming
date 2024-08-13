bb5f0cb62a67e23012fa2d788650257fdb7b2e25
ConanODoyle <>
2024-08-12 23:18:09 -0400

    Delete incomplete files off of main

--- scripts/community/server.cs
+++
@@ -1,5 +0,0 @@
-//General design:
-
-//Daily challenge: some selection of crops: deposit past a threshold to win bux and tix
-//Weekly challenge: smattering of crops: deposit past a threshold to win farmicoin
-
