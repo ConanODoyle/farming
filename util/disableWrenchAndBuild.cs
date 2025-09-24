2e599c7ca84c3808bb5c00d148a05b53b37e3c41
ConanODoyle <samuel.w.sze@gmail.com>
2025-09-24 16:59:22 -0400

    Another fix for people trying to cause problems

--- util/disableWrenchAndBuild.cs
+++ util/disableWrenchAndBuild.cs
@@ -175,7 +175,7 @@ package disableWrenchData
 			%db = %pl.tempBrick.getDatablock();
 			if ((%db.category $= "Baseplates" && %db.subCategory !$= "Plain") || %db.subCategory $= "Drinks" || %db.subCategory $= "Holes"
 				|| %db.category $= "Zonebricks" || %db.uiName $= "Treasure Chest" || %db.isShopLot || %db.isLot || %db.isTeledoor || %db.isIllegal
-				|| %db.uiName $= "Waving Flag" || %db.uiName $= "Still Flag")
+				|| %db.uiName $= "Waving Flag" || %db.uiName $= "Still Flag" || %db.uiName $= "")
 			{
 				if (%db.subCategory $= "Cube" && getWord(%db.uiname, 0) < 16)
 				{
